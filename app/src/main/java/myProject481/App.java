6f17cb5f28ecadc8eaeae403d8e866af8011a954
can.kocyigitoglu <kocyigitoglucan@gmail.com>
2020-11-25 11:33:43 +0300

    Set the port number from the environment variable

--- app/src/main/java/myProject481/App.java
+++ app/src/main/java/myProject481/App.java
@@ -7,11 +7,12 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
-import java.lang.System.Logger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.LogManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import spark.ModelAndView;
   import spark.template.mustache.MustacheTemplateEngine;
@@ -33,13 +34,12 @@ import spark.ModelAndView;
     }
 
     public static void main(String[] args) {
+      Logger logger = LogManager.getLogger(App.class);
+      int port = Integer.parseInt(System.getenv("PORT"));
+      port(port);
+      logger.error("Current port number:" + port);
+
         port(getHerokuAssignedPort());
-        Logger logger = LogManager.getLogger(App.class);
-
-        int port = Integer.parseInt(System.getenv("PORT"));
-        port(port);
-        logger.error("Current port number:" + port);
-
 
         get("/", (req, res) -> "Hello, World");
 
